383a5c5ab231466bf2577b0b706cea764f1ac340
JAugusto-Moraes <231026429@aluno.unb.br>
2023-09-17 19:50:44 -0300

    Adicionei o banco de dados no Django e fiz o primeito migrate

--- setup/settings.py
+++ setup/settings.py
@@ -81,11 +81,14 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+              'ENGINE': 'django.db.backends.mysql',
+              'NAME': 'verdepantano',
+              'USER': 'Admin',
+              'PASSWORD': str(os.getenv('DATABASE_KEY')),
+              'HOST': 'localhost',
+              'PORT': '3306',
+          }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -124,7 +127,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'setup/static')
+    os.path.join(BASE_DIR, 'setup\static')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
