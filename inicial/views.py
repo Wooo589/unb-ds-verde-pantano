8cad169d005028bb199245a41281c483afd4757b
JAugusto-Moraes <231026429@aluno.unb.br>
2023-10-01 19:14:26 -0300

    Alteração na página locais de atendimento

--- inicial/views.py
+++ inicial/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from inicial.models import Hospital
 
 def index(request):
     return render(request, ('inicial/index.html'))
 
 def locais_de_atendimento(request):
-    return render(request, ('inicial/locais_de_atendimento.html'))
+    hospitais = Hospital.objects.order_by("nome")
+    return render(request, 'inicial/locais_de_atendimento.html', {"hospitais": hospitais})
