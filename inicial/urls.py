57e8493d0aed7df125f6695212e357f833420eb1
JAugusto-Moraes <231026429@aluno.unb.br>
2023-10-01 17:59:17 -0300

    Adicionadas as páginas feitas pelo Pedro

--- inicial/urls.py
+++ inicial/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from inicial.views import index
+from inicial.views import index, locais_de_atendimento
 
 urlpatterns = [
     path('', index, name='index'),
+    path("atendimento", locais_de_atendimento, name='locais_de_atendimento')
 ]
--- inicial/views.py
+++ inicial/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, ('inicial/index.html'))
+
+def locais_de_atendimento(request):
+    return render(request, ('inicial/locais_de_atendimento.html'))
