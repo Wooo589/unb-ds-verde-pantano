ee51ee763b65a82094afa4ad1beac54804385c50
JAugusto-Moraes <231026429@aluno.unb.br>
2023-10-02 15:59:50 -0300

    Adicionei a funcionalidade de busca nos locais de atendimento

--- inicial/urls.py
+++ inicial/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from inicial.views import index, locais_de_atendimento
+from inicial.views import index, locais_de_atendimento, buscar
 
 urlpatterns = [
     path('', index, name='index'),
-    path("atendimento", locais_de_atendimento, name='locais_de_atendimento')
+    path("atendimento", locais_de_atendimento, name='locais_de_atendimento'),
+    path("atendimento/", buscar, name='buscar')
 ]
--- inicial/views.py
+++ inicial/views.py
@@ -7,3 +7,14 @@ def index(request):
 def locais_de_atendimento(request):
     hospitais = Hospital.objects.order_by("nome")
     return render(request, 'inicial/locais_de_atendimento.html', {"hospitais": hospitais})
+
+def buscar(request):
+    hospitais = Hospital.objects.order_by("nome")
+
+    if "buscar" in request.GET:
+        nome_a_buscar = request.GET['buscar']
+
+        if nome_a_buscar:
+            hospitais = hospitais.filter(nome__icontains=nome_a_buscar)
+
+    return render(request, "inicial/buscar.html", {"hospitais": hospitais})
