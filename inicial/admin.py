f00abf824a8c3caf73a899b9a27e1df0506fe486
JAugusto-Moraes <231026429@aluno.unb.br>
2023-10-07 11:26:41 -0300

    Adicionei UFs ao banco de dados

--- inicial/admin.py
+++ inicial/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from inicial.models import Hospital
 
 class ListandoHospitais(admin.ModelAdmin):
-    list_display = ("id", "nome", "cep", "categoria")
+    list_display = ("id", "nome", "uf", "categoria")
     ordering = ["id"]
     search_fields = ("nome",)
-    list_filter = ("categoria",)
+    list_filter = ("categoria", "uf")
     list_per_page = 10
 
 admin.site.register(Hospital, ListandoHospitais)
--- inicial/static/js/script-select-menu.js
+++ inicial/static/js/script-select-menu.js
@@ -4,7 +4,7 @@ searchInp = wrapper.querySelector("input"),
 options = wrapper.querySelector(".options");
  
 //array of some UF.
-let UF = ["AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"];
+let UF = ["AC","AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"];
 
 function addUF(selectedUF) {
     options.innerHTML = "";
